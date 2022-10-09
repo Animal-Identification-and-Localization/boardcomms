4d014e1b22073b2cc1812f0cb5f11f05ff02dd77
Shane Raible <ssraible@gmail.com>
2022-10-09 17:22:22 -0400

    coral pb out

--- coral_coms/coms_py/coral_pb_out.py
+++ coral_coms/coms_py/coral_pb_out.py
@@ -1,12 +1,41 @@
-from periphery import GPIO
+# from periphery import GPIO
 
-led = GPIO("/dev/gpiochip0", 11, "out")
+# led = GPIO("/dev/gpiochip0", line=11, direction="out", bias="pull_up")
+
+# try:
+#     while True:
+#         led.write(True)
+# finally:
+#     led.write(False)
+#     led.close()
+
+from periphery import SysfsGPIO
+from periphery import GPIO
+import time
+
+# led = GPIO("/dev/gpiochip0", 22 ,"out") 
+led = GPIO(
+    "/dev/gpiochip0", 22, 
+    "out", edge='none', 
+    bias='default', drive='open_source', 
+    inverted=True, label=None
+)
+
+# led = SysfsGPIO(409,"low") 
 
 try:
+        # if led.read(): print("High")
     while True:
+        led.write(False)
+        time.sleep(2)
         led.write(True)
+        time.sleep(2)
+
+
+        # print('hi')
 finally:
     led.write(False)
     led.close()
+
 
 
