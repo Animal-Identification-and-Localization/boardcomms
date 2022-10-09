5ede3c2ec52ff3100df6b8b5f2c6db314cc4320d
Shane Raible <ssraible@gmail.com>
2022-10-09 19:55:34 -0400

    i2c v1

--- coral_coms/coms_py/coral_pb_out.py
+++ coral_coms/coms_py/coral_pb_out.py
@@ -11,34 +11,41 @@
 
 from periphery import SysfsGPIO
 from periphery import GPIO
+from periphery import I2C
 import time
-import boardcomms_pb2
+
+if 0:
+    # led = GPIO("/dev/gpiochip0", 22 ,"out") 
+    led = GPIO(
+        "/dev/gpiochip0", 22, 
+        "out", edge='none', 
+        bias='default', drive='open_source', 
+        inverted=True, label=None
+    )
+
+    # led = SysfsGPIO(409,"low") 
+
+    try:
+            # if led.read(): print("High")
+        while True:
+            led.write(False)
+            time.sleep(2)
+            led.write(True)
+            time.sleep(2)
 
 
-
-# led = GPIO("/dev/gpiochip0", 22 ,"out") 
-led = GPIO(
-    "/dev/gpiochip0", 22, 
-    "out", edge='none', 
-    bias='default', drive='open_source', 
-    inverted=True, label=None
-)
-
-# led = SysfsGPIO(409,"low") 
-
-try:
-        # if led.read(): print("High")
-    while True:
+            # print('hi')
+    finally:
         led.write(False)
-        time.sleep(2)
-        led.write(True)
-        time.sleep(2)
+        led.close()
 
 
-        # print('hi')
-finally:
-    led.write(False)
-    led.close()
+i2c1 = I2C("/dev/i2c-3")
+# Read byte at address 0x100 of EEPROM at 0x50
+for i in range(0,101):
+    msgs = [I2C.Message([i, i, i]), I2C.Message([0x00], read=True)]
+    i2c1.transfer(404, msgs)
 
+print("0x100: 0x{:02x}".format(msgs[1].data[0]))
 
-
+i2c1.close()
